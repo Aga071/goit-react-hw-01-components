65b994e5c9ce247dba237683a14de30e739fa640
Agata Warzyszyńska <agata.wlazlak@gmail.com>
2023-10-14 14:31:23 +0200

    goit-react-hw-01-v3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,12 +14,13 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        // position: ,
       }}
     >
       <>
