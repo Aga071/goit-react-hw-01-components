58c5032e6773377e65568932158a8bcd48bb6160
Agata Warzyszyńska <agata.wlazlak@gmail.com>
2023-10-25 23:00:38 +0200

    goit-react-hw-01-v5

--- src/components/TransactionHistory.jsx
+++ src/components/TransactionHistory.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import css from '../css/TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
-  console.log(items);
   return (
     <table className={css['transaction-history']}>
       <thead>
